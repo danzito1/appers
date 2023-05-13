5354a59deb4fd34fc6dd9e13335f4939a153d75d
danzito1 <108548505+danzito1@users.noreply.github.com>
2023-05-12 21:26:00 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 window.onload = function() {
     var top = document.getElementById('top');
     var center = document.getElementById('center');
+    var divGo = document.getElementByClassName('bt');
     
     if (!/Android|iPhone|iPod|iPad|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
         top.style.width = "100vh";
         top.style.height = "90vh";
         center.setAttribute("src", "IMG_20230512_134202.png");
         center.style.marginTop = "40vh";
+        divGo.style.width = "30vh";
     }
 }
