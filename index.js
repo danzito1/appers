b203d358091a2717095bd4a10900f3bb4a3303f7
danzito1 <108548505+danzito1@users.noreply.github.com>
2023-05-17 14:19:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,21 +7,27 @@
     const lang = navigator.language;
     lang = lang.split('-')[0];
     
-    if (navigator.userAgent === "webOS") {
+    if (lang === "en") {
+        window.location.href = "en.html";
+    } else if (lang === "es") {
+        window.location.href = "es.html";
+    }
+
+    isPC();
+    
+    go.addEventListener('click', function() {
+        window.location.assign("https://danzito1.github.io/appers/home");
+    });
+
+function isPC() {
+    var user = navigator.userAgent.toLowerCase();
+    var isMobile = /iphone|ipad|ipod|android|blackberry|opera mini|iemobile|mobile|tablet/i.test(user);
+    
+    if (!isMobile) {
         top.style.width = "100vh";
         top.style.height = "90vh";
         centerImg.setAttribute("src", "IMG_20230512_134202.png");
         centerImg.style.marginTop = "40vh";
         divGo.style.width = "30vh";
     }
-    
-    if (lang === "en") {
-        window.location.href = "en.html";
-    } else if (lang === "es") {
-        window.location.href = "es.html";
-    }
-    
-    go.addEventListener('click', function() {
-        window.location.assign("https://danzito1.github.io/appers/home");
-    });
-
+}
